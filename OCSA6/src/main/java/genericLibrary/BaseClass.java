f31455b45c687032495a9f8798d42059e2598860
paidisyam <paidisyam076@gmail.com>
2022-04-13 11:46:48 +0530

    done with module

---
+++ OCSA6/src/main/java/genericLibrary/BaseClass.java
@@ -0,0 +1,49 @@
+package genericLibrary;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+
+
+public class BaseClass {
+	public WebDriver driver;
+	public DataUtility du=new DataUtility();
+	public static WebDriver listenerDriver;
+	@BeforeClass(alwaysRun=true)
+	public void launchBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver","./src/main/resources/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		listenerDriver=driver;
+	}
+	
+	@BeforeMethod(alwaysRun=true)
+	public void loginApp() throws IOException
+	{
+		driver.get(du.getDatafromProperties("url"));
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+	}
+	@AfterMethod(alwaysRun=true)
+	public void logoutApp() 
+	{
+		driver.findElement(By.id("logoutLink")).click();
+	}
+	@AfterClass(alwaysRun=true)
+	public void closeBrowser()
+	{
+		driver.close();
+	}
+
+}
---
+++ OCSA6/src/test/java/testScript/CreateCustomerTest.java
@@ -0,0 +1,30 @@
+package testScript;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import genericLibrary.BaseClass;
+
+public class CreateCustomerTest extends BaseClass{
+	@Test
+	public void createCustomer() throws EncryptedDocumentException, IOException
+	{
+	Random r=new Random();
+	int num=r.nextInt(10000);
+	driver.findElement(By.id("container_tasks")).click();
+	driver.findElement(By.cssSelector(".title.ellipsis")).click();
+	driver.findElement(By.className("createNewCustomer")).click();
+	driver.findElement(By.className("newNameField"))
+	.sendKeys(num+du.getDatafromExcelsheet("Sheet1", 0, 0));
+	driver.findElement(By.cssSelector("[placeholder='Enter Customer Description']"))
+	.sendKeys("done");
+	driver.findElement(By.xpath("//div[text()='Create Customer']")).click();
+	System.out.println("customer created");
+	
+}
+
+}
